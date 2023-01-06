e3eafe788d68afa587cc48d186d288ef0b26304b
Monika.Kynnapas <monikakynnapas@hot.ee>
2023-01-06 11:35:34 +0200

    Added Leaderboard.py and GameTime.py

---
+++ Leaderboard.py
@@ -0,0 +1,10 @@
+new *
+class Leaderboard:
+
+    new *
+    def __init__(self, date, name, word, misses, time):
+        self.date = date
+        self.name = name
+        self.word = word
+        self.misses = misses
+        self.time = time
